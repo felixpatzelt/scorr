7e61d007fa60d581cd42a8ff4e56257f784f136f
Felix Patzelt <mail@felixpatzelt.com>
2025-08-04 22:10:47 +0200

    fix: remove undocumented incompatible code

--- scorr/__init__.py
+++ scorr/__init__.py
@@ -1,9 +1,2 @@
 from .corr2 import *
 from .corr3 import *
-import imp
-
-def __reload_submodules__():
-    from . import helpers
-    imp.reload(helpers)
-    imp.reload(corr2)
-    imp.reload(corr3)
